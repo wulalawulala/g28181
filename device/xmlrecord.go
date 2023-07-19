3f76b68943512bd450712bdeb8a985575ec7ec91
qm <123736640@qq.com>
2023-07-19 15:33:32 +0800

    1、修改录像功能

--- device/xmlrecord.go
+++ device/xmlrecord.go
@@ -23,12 +23,19 @@ type RecordItem struct {
 	Secrecy   int    `xml:"Secrecy" bson:"Secrecy" json:"Secrecy"`
 	Type      string `xml:"Type" bson:"Type" json:"Type"`
 }
+
+type RecordLists struct {
+	Num  int          `xml:"Num,attr"`
+	Item []RecordItem `xml:"Item"`
+}
+
 type MessageRecordInfoRsp struct {
-	CmdType  string       `xml:"CmdType"`
-	SN       int          `xml:"SN"`
-	DeviceID string       `xml:"DeviceID"`
-	SumNum   int          `xml:"SumNum"`
-	Item     []RecordItem `xml:"RecordList>Item"`
+	CmdType  string `xml:"CmdType"`
+	SN       int    `xml:"SN"`
+	DeviceID string `xml:"DeviceID"`
+	SumNum   int    `xml:"SumNum"`
+	// Item     []RecordItem `xml:"RecordList>Item"`
+	Records RecordLists `xml:"RecordList"`
 }
 
 //2006-01-02T15:04:05
